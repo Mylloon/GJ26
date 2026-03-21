26d9efb0ea456e71d2629e6ba10ac96be0847a1e
Mylloon <kennel.anri@tutanota.com>
2026-03-21 13:35:54 +0100

    typo

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ const SPEED = 5.0
 
 @onready var camera_rotation = $"../../SubViewportContainer/SubViewport/Camera3D".get_rotation()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var inputs = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (
 						transform.basis * Vector3(inputs.x, 0, inputs.y)
