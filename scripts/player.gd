a38c45c78a66330ea4890e10babd71907a040c73
Mylloon <kennel.anri@tutanota.com>
2026-03-21 13:35:01 +0100

    fix controller and player movements

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,13 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 
+@onready var camera_rotation = $"../../SubViewportContainer/SubViewport/Camera3D".get_rotation()
 
 func _physics_process(delta):
 	var inputs = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(inputs.x, 0, inputs.y)).normalized()
+	var direction = (
+						transform.basis * Vector3(inputs.x, 0, inputs.y)
+					).rotated(Vector3.UP, camera_rotation.y).normalized()
 
 	if direction:
 		velocity.x = direction.x * SPEED
