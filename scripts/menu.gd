95893f309d8dee292ff290b89dc31baa90e36a75
Mylloon <kennel.anri@tutanota.com>
2026-03-21 12:40:29 +0100

    clean+better menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,3 +7,7 @@ func _on_quit_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	Context.switch_scene("res://scenes/settings.tscn")
+
+
+func _on_play_pressed() -> void:
+	Context.switch_scene("res://scenes/kitchen.tscn")
