9fc83b00bd122c16e8550147e1817b7d8570025f
Mylloon <kennel.anri@tutanota.com>
2026-03-21 10:19:19 +0100

    player+kitchen basis

---
+++ scripts/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+
+func _physics_process(delta):
+	var inputs = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(inputs.x, 0, inputs.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
