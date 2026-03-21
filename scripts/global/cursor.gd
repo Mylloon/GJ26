809bc9a3388e7611a0a97034afccc14e20ae0955
Mylloon <kennel.anri@tutanota.com>
2026-03-21 16:05:50 +0100

    taches, cursor

---
+++ scripts/global/cursor.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var arrow = load("res://assets/cursor/cursor_idle.png")
+var hover = load("res://assets/cursor/cursor_hover.png")
+var clicked = load("res://assets/cursor/cursor_clic.png")
+
+
+func _ready():
+	Input.set_custom_mouse_cursor(hover, Input.CURSOR_POINTING_HAND)
+	Input.set_custom_mouse_cursor(clicked, Input.CURSOR_DRAG)
+
+	Input.set_custom_mouse_cursor(arrow, Input.CURSOR_ARROW)
+
+
+func _process(delta):
+	var click = "clic_gauche"
+	if Input.is_action_just_pressed(click):
+		Input.set_custom_mouse_cursor(clicked, Input.CURSOR_ARROW)
+		
+	if Input.is_action_just_released(click):
+		Input.set_custom_mouse_cursor(arrow, Input.CURSOR_ARROW)
