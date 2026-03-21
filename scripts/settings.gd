e7fcb9c8f6caafc23ded3eb2ddebaa1b5b0d5bdc
Mylloon <kennel.anri@tutanota.com>
2026-03-21 23:57:53 +0100

    better volume slider

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,34 +1,37 @@
-extends GridContainer
+extends VBoxContainer
 
-@onready var volume_slider = $Volume/HSlider
+@onready var volume_bar = $Volume/Slider
 @onready var volume_info = $Volume/Info
 
 
 func _ready():
-	volume_slider.set_value(AudioHandler.get_volume() * 100)
-
+	volume_bar.value = AudioHandler.get_volume() * 100
+	_update_info(volume_bar.value)
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		_on_return_button_pressed()
 
-
-func _on_volume_slider_value_changed(value: float) -> void:
+func _on_slider_value_changed(value: float) -> void:
 	AudioHandler.set_volume(value / 100)
+	_update_info(value)
+
+func _on_slider_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and not event.pressed:
+		_save_volume()
+	elif event is InputEventMouseMotion and event.button_mask & MOUSE_BUTTON_MASK_LEFT:
+		var ratio = 1.0 - clampf(event.position.y / volume_bar.size.y, 0.0, 1.0)
+		volume_bar.set_value(ratio * 100)
+
+func _save_volume() -> void:
+	ConfigHandler.save_setting(
+		ConfigHandler.SETTINGS.audio.name,
+		ConfigHandler.SETTINGS.audio.elements.master.name,
+		volume_bar.value,
+	)
+
+func _update_info(value: float) -> void:
 	volume_info.set_text(str(value).pad_decimals(0) + "%")
-
-
-func _on_volume_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed:
-		(
-			ConfigHandler
-			. save_setting(
-				ConfigHandler.SETTINGS.audio.name,
-				ConfigHandler.SETTINGS.audio.elements.master.name,
-				volume_slider.get_value(),
-			)
-		)
-
 
 func _on_return_button_pressed() -> void:
 	Context.return_to_previous()
