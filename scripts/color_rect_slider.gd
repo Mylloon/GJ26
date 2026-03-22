4ef556d8a952e79ff87a84ede55e8ba8a2e21259
Mylloon <kennel.anri@tutanota.com>
2026-03-22 18:13:01 +0100

    fmt

--- scripts/color_rect_slider.gd
+++ scripts/color_rect_slider.gd
@@ -1,13 +1,15 @@
 extends ColorRect
 
-var droite = true 
+var droite = true
 var min_bar = 445
 var max_bar = 790
 var speed = 400
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass  # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,12 +21,10 @@ func _process(delta: float) -> void:
 		position -= Vector2(1, 0) * speed * delta
 		if position.x <= min_bar:
 			droite = true
-	
+
 	if Input.is_action_just_pressed("left_click"):
 		if position.x < 710 and position.x > 655:
 			Context.return_to_previous(true)
 			print(true)
-		else: 
+		else:
 			print(false)
-			
-	
